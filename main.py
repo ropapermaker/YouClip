2139f82098f1fe08e2b2dd161c54c01077a3a60a
ador <81928644+adornerz@users.noreply.github.com>
2021-07-26 21:10:45 +0200

    version 1.0.1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import eel
 import youtube_dl
 import requests
 import os
+import random
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from datetime import datetime
 
@@ -18,72 +19,75 @@ def check_video_url(url):
                 return("invalid")
             else:
                 return("valid")
-                
+
         except requests.exceptions.MissingSchema:
             return("schema")
         except:
             return("unknownerror")
-        
+
     else:
         print("youtube.com in link not found")
         return("notyt")
-
+randomNumbers = 0
+vidTitle = f"video{randomNumbers}.mp4"
 @eel.expose
 def download_video(videoURL):
+    randNums = random.randint(1000, 9999)
+    randomNumbers = randNums
+    print(vidTitle)
     try:
-        print(videoURL)
-        os.system(f"youtube-dl -o 'video6932.mp4' " + videoURL)
+        os.system(f"youtube-dl --format 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' -o '{vidTitle}' {videoURL}")
         return("downloaded")
     except:
         return("error")
-    
+
 
 @eel.expose
 def crop_video(startmin, startsec, endmin, endsec):
-    
+
     seconds = [startmin, startsec, endmin, endsec]
     intval = []
-    
+
     for second in seconds:
         if second == '':
             second = 0
         intval.append(int(second))
-    
+
     for second in intval:
         if second > 60:
-            os.remove('video6932.mp4')
+            os.remove(vidTitle)
             return("greater60")
-    
+
     total = sum(intval)
-    
+
     if total == 0:
-        os.remove('video6932.mp4')
+        os.remove(vidTitle)
         return("empty")
     else:
         pass
-    
+
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    
+
     secondstart = intval[0] * 60 + intval[1]
     secondend = intval[2] * 60 + intval[3]
-    
 
     if (secondstart < secondend):
         try:
-            ffmpeg_extract_subclip("video6932.mp4", secondstart, secondend, targetname=f"croppedVideo_{current_time}.mp4")
-            os.remove('video6932.mp4')
+            print(f"\n\nVideo Title: {vidTitle}\n\n")
+            ffmpeg_extract_subclip(vidTitle, secondstart, secondend, targetname=f"croppedVideo_{current_time}.mp4")
+            os.remove(vidTitle)
             return(f"croppedVideo_{current_time}.mp4")
         except:
-            os.remove('video6932.mp4')
+            os.remove(vidTitle)
             return("fail")
     elif (secondstart == secondend):
-        os.remove('video6932.mp4')
+        os.remove(vidTitle)
         return("equal")
-    
+
     elif (secondstart > secondend):
-        os.remove('video6932.mp4')
+        os.remove(vidTitle)
         return("greaterstart")
-    
-    
+
+
 eel.start('index.html', size = (1000, 600))
